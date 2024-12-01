f5dcf24a2e575eaab22275f4e4dbff125a79846d
Aung Chan Myae <aungchanmyae10997@gmail.com>
2024-12-01 23:03:04 +0700

    Finished First Part

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+/**
+ * @copyright 2024 Aung Chan Myae
+ * @license Apache-2.0
+ */
+
+/**
+ * components
+ */
+
+import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+      <Hero />
+        </main>
+    </div>
+  )
+}
+
+export default App
